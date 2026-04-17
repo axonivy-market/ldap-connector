e5b06095ea25d4764e63f141dcba95676c63be49
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2026-04-17 08:58:19 +0700

    Update LdapFilterUtilTest.java

--- ldap-connector-test/src_test/com/axonivy/connector/ldap/test/LdapFilterUtilTest.java
+++ ldap-connector-test/src_test/com/axonivy/connector/ldap/test/LdapFilterUtilTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import com.axonivy.connector.ldap.util.LdapFilterUtil;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LdapFilterUtilTest {
 
